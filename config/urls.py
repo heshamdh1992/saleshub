f663d593f88d6503d8b9efb2a5b34c718f50c803
Hesham Aldaher <h.aldaher.musa@gmail.com>
2026-04-13 09:45:14 +0300

    Fix media serving on production

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path ,re_path
 from apps.core.views import DashboardView
 from apps.core.views import ServiceWorkerView
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -21,5 +22,6 @@ urlpatterns = [
     path("service-worker.js", ServiceWorkerView.as_view(), name="service_worker"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
